6d1debf08adf0b3412203bf0eee2b6f6eb77fb53
webdevelopmentdept-dotcom <webdevelopmentdept@gmail.com>
2025-11-10 16:06:55 +0530

    delete issues in backend

--- models/HrApplicant.js
+++ models/HrApplicant.js
@@ -4,7 +4,6 @@ const hrApplicantSchema = new mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   phone: { type: String, required: true },
-  address: { type: String, required: true },
   jobTitle: { type: String, required: true },
   resumeUrl: { type: String },
   createdAt: { type: Date, default: Date.now },
