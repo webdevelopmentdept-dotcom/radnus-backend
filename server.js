780686c67bc1112bbcb8bbe4c99e110f80ef1719
bemaxzsolutions-bit <bemaxzsolutions@gmail.com>
2026-04-15 16:49:09 +0530

    Feedbacknomination file name changed

--- server.js
+++ server.js
@@ -84,7 +84,7 @@ const variablePayRoutes = require("./routes/variablePay");
 const feedbackSubmission = require("./routes/feedbacksubmission");
 
 const feedbackTaskRoutes = require("./routes/feedbackTask.routes");
-const feedbackNominationRoutes = require("./routes/feedbackNomination");
+const feedbackNominationRoutes = require("./routes/FeedbackNomination");
 const employeeAwardRoutes = require("./routes/employeeAwards");
 const esopRoutes = require("./routes/esop");
 const impactBonusRoutes = require("./routes/impactBonus");
